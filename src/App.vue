4595597af151a561caf4d4da382604b3057e11f5
ahmed ali shams <ahmedalishams@outlook.com>
2020-08-23 00:51:02 +0200

    working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card mb-4 box-shadow">
+    <div class="card-header">
+      <h4 class="my-0 font-weight-normal">Currency Coverter</h4>
+    </div>
+    <div class="card-body">
+      <display-convert class="container" />
+      <currency-form class="container" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import currencyForm from "./components/currencyForm";
+  import displayConvert from "./components/displayConvert";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    currencyForm,
+    displayConvert
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
